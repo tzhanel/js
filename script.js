fe3ebb8d5dff39164f33b192699987b2444fe205
tzhanel <janelthebest@gmail.com>
2026-05-14 20:43:00 +0500

    /

--- script.js
+++ script.js
@@ -1,59 +1,44 @@
-const cards = document.getElementById("cards");
-const btn = document.getElementById("add-card")
-const emojis = ["😀", "😎", "🤓", "😴", "🤖", "👻", "💩", "🌈", "🔥", "✨", "🍕", "⚡️"];
+console.log(colorPicker.value)
 
-let counter = 0;
+  const swatch = document.createElement("div")
+  swatch.className = "swatch"
+  console.log(swatch)
 
-btn.addEventListener("click", () => {
-    const card = document.createElement("div")
-    card.classList.add("card");
-    if (counter >= emojis.length - 1) {
-        counter = 0;
-    }
-    card.textContent = emojis[counter]
-    counter++;
-    cards.appendChild(card)
+  const colorDiv = document.createElement("div")
+  swatch.appendChild(colorDiv)
+  
+  swatchGrid.appendChild(swatch)
+  console.log(colorPicker.value)
 
-    card.addEventListener("click", () => {
-        card.remove();
-    })
+  const swatch = document.createElement("div")
+  swatch.className = "swatch"
+  console.log(swatch)
+
+  const colorDiv = document.createElement("div")
+  swatch.appendChild(colorDiv)
+  
+  swatchGrid.appendChild(swatch)
+  addBtn.addEventListener("click", function() {
+  const hex = colorPicker.value
+
+  const swatch = document.createElement("div")
+  swatch.className = "swatch"
+  console.log(swatch)
+
+  const colorDiv = document.createElement("div")
+  colorDiv.className = "swatch-color"
+  colorDiv.style.backgroundColor = hex
+  swatch.appendChild(colorDiv)
+
+  swatchGrid.appendChild(swatch)
+  
+
+  
+
+
+  //  <div class="swatch-grid" id="swatch-grid">
+     // colorDiv = document.createElement("div")
+  // </div>
+
+
 })
-
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    console.log("response => ", response);
-    const data = await response.json();
-    console.log("data => ", data);
-
-    return data;
-} catch (error) {
-  console.log(error)
-}
-}
-
-const dogBtn = document.getElementById("dogBtn");
-const catBtn = document.getElementById("catBtn");
-const output = document.getElementById("output");
-dogBtn.addEventListener("click", async () => {
-    const data = await getData("https://dog.ceo/api/breeds/image/random");
-    const img = document.createElement("img");  
-    img.src = data.message;
-    img.style.width = "200px";
-    // output.innerHTML = "";
-    output.appendChild(img);
-}
-);
-catBtn.addEventListener("click", async () => {
-    const data = await getData("https://api.thecatapi.com/v1/images/search");
-    const img = document.createElement("img");
-    img.src = data[0].url;
-    img.style.width = "200px";
-    output.innerHTML = "";
-    output.appendChild(img);
-}
-);
-
-
-// https://dog.ceo/api/breeds/image/random
-// https://api.thecatapi.com/v1/images/search
